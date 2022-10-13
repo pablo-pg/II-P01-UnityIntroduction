4f0820b0f2c573368ad95ea58b774747aebcdde3
Pablo Pérez González <alu0101318318@ull.edu.es>
2022-10-13 17:16:04 +0100

    Código

---
+++ cubo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola, soy un cubo!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ esfera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class esfera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola, soy una esfera!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
